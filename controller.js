f4330556902adc4edb9d4b4a9d3e5c861765c3a3
shannon <shannonpetersen33@gmail.com>
2026-06-10 17:26:48 +0200

    Complete controller.js and add display-controller.js

--- controller.js
+++ controller.js
@@ -4,10 +4,47 @@ import { board } from "./gameboard.js";
 //player objects
 const players = [createPlayer("Player 1", "X"), createPlayer("Player 2", "O")];
 
-//active player
-let activePlayer = players[0];
+let activePlayer;
+
+function starterPlayer() {
+  players.forEach((player) => {
+    const starter = players.findIndex((start) => start.getMarker() === "X");
+    activePlayer = players[starter];
+  });
+}
+
+//player switcher
+function playerSwitcher() {
+  if (activePlayer == players[0]) {
+    activePlayer = players[1];
+  } else if (activePlayer == players[1]) {
+    activePlayer = players[0];
+  }
+}
 
 //turn dictator
+function turnDictator() {
+  const status = winnerChecker();
+  switch (status) {
+    case "changeTurn":
+      playerSwitcher();
+      break;
+
+    case "tie":
+      board.reset();
+      console.log("It's a tie - Game Over");
+      break;
+
+    case "winner":
+      board.reset();
+      console.log("Game Over");
+      break;
+
+    default:
+      playerSwitcher();
+      break;
+  }
+}
 
 //winner checker
 function winnerChecker() {
@@ -30,8 +67,24 @@ function winnerChecker() {
       activePlayer.getMarker() == currentBoard[winner.second] &&
       activePlayer.getMarker() == currentBoard[winner.third]
     ) {
-      console.log("winner");
-      return true;
+      return "winner";
     }
   }
+
+  if (currentBoard.every((val) => val !== "")) {
+    return "tie";
+  }
+  return "changeTurn";
 }
+
+//marker placer
+function placeMarker(position) {
+  const currentBoard = board.getter();
+  if (currentBoard[position] == "") {
+    board.place(position, activePlayer.getMarker());
+    turnDictator();
+  } else {
+    console.log("try another spot");
+  }
+}
+starterPlayer();
